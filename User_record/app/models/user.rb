80ebf4b2a4fc365b67a58dc556e1119ad9158c04
Anchit Mishra <anchit0810@gmail.com>
2026-04-03 15:11:01 +0530

    changed my DB from sqlite3 to posgresql

--- User_record/app/models/user.rb
+++ User_record/app/models/user.rb
@@ -9,21 +9,21 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[^@\s]+@[^@\s]+\.[^@\s]+\z/
 
 
-  validates :first_name , presence: true , length: {maximum: 20}, format: {with: /\A[a-zA-Z]+\z/},on: :create , on: :update # if: :will_save_change_to_first_name?
+  validates :first_name , presence: true , length: {maximum: 20}, format: { with: /\A[a-zA-Z]+\z/ } , on: :create , on: :update # if: :will_save_change_to_first_name?
   
-  validates :last_name , presence: true , length: {maximum: 20}, format: {with: /\A[a-zA-Z]+\z/}, on: :create, on: :update
+  validates :last_name , presence: true , length: {maximum: 20}, format: { with: /\A[a-zA-Z]+\z/ } , on: :create , on: :update
   
-  validates :email , presence: true , uniqueness: { case_sensitive: false } , format: { with: VALID_EMAIL_REGEX}, on: :create #, on: :update
+  validates :email , presence: true , uniqueness: { case_sensitive: false } , format: { with: VALID_EMAIL_REGEX} , on: :create #, on: :update
 
   validates :country_code , presence: true , length: {is: 3} , format: {with: /\A\+\d{1,3}\z/ }, on: :create #,on: :update
   
-  validates :mobile_number , presence: true ,length: {is: 10} , format: {with: /\A[2-9]{1}[0-9]{9}\z/ }, on: :create #, on: :update
+  validates :mobile_number , presence: true ,length: {is: 10} , format: { with: /\A[2-9]{1}[0-9]{9}\z/ } , on: :create #, on: :update
   
-  validates :country , presence: true , format: {with: /\A[a-zA-Z]+\z/},on: :create ,on: :update
+  validates :country , presence: true , format: { with: /\A[a-zA-Z]+\z/ } , on: :create , on: :update
   
-  validates :state , presence: true , format: {with: /\A[a-zA-Z]+\z/},on: :create , on: :update
+  validates :state , presence: true , format: { with: /\A[a-zA-Z]+\z/} , on: :create , on: :update
   
-  validates :city , presence: true , format: {with: /\A[a-zA-Z]+\z/},on: :create ,on: :update
+  validates :city , presence: true , format: { with: /\A[a-zA-Z]+\z/ } , on: :create , on: :update
 
 
   private
